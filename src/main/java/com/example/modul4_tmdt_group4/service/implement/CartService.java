55773c73ea3ff4aac1a768ce6f9c1987d197bebc
admin <admin@MacBook-Air-cua-admin.local>
2023-10-03 21:27:55 +0700

    controller cart

--- src/main/java/com/example/modul4_tmdt_group4/service/implement/CartService.java
+++ src/main/java/com/example/modul4_tmdt_group4/service/implement/CartService.java
@@ -1,35 +1,40 @@
 package com.example.modul4_tmdt_group4.service.implement;
 
 import com.example.modul4_tmdt_group4.model.Cart;
+import com.example.modul4_tmdt_group4.repository.ICartRepository;
 import com.example.modul4_tmdt_group4.service.ICartService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
 public class CartService implements ICartService {
+    @Autowired
+    ICartRepository cartRepository;
+
     @Override
     public Iterable<Cart> findAll() {
-        return null;
+        return cartRepository.findAll();
     }
 
     @Override
     public Optional<Cart> findById(Long id) {
-        return Optional.empty();
+        return cartRepository.findById(id);
     }
 
     @Override
     public void create(Cart cart) {
-
+        cartRepository.save(cart);
     }
 
     @Override
     public void update(Cart cart) {
-
+        cartRepository.save(cart);
     }
 
     @Override
     public void delete(Long id) {
-
+        cartRepository.deleteById(id);
     }
 }
