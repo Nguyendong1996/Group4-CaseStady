410ebea69e62592aa0ddd89ac2a80dc7fc5be4da
LeDuyKhanh <khanh.ld2208@gmail.com>
2023-10-06 15:22:56 +0700

    Find by all condition

--- frontend/FE/search.js
+++ frontend/FE/search.js
@@ -1,73 +1,51 @@
 $.ajax({
-        type: "GET",
-        url: "http://localhost:8080/api/provider",
-        success: function (data) {
-            console.log(data);
-            let content = "";
-            for (let i = 0; i < data.length; i++) {
-                content += `<option value=${data[i]}>${data[i].name}</option>`
-            }
-            document.getElementById("P_id").innerHTML = content;
-        },
-        error: function (err) {
-            console.log(err)
-            // lỗi
+    type: "GET",
+    url: "http://localhost:8080/api/provider",
+    success: function (data) {
+        console.log(data);
+        let content = "";
+        for (let i = 0; i < data.length; i++) {
+            content += `<option></option><br><option value=${data[i].id}>${data[i].name}</option>`
         }
-    });
-
-$.ajax({
-        type: "GET",
-        url: "http://localhost:8080/api/category",
-        success: function (data) {
-            console.log(data);
-            let content = "";
-            for (let i = 0; i < data.length; i++) {
-                content += `<option value=${data[i]}>${data[i].name}</option>`
-            }
-            document.getElementById("C_id").innerHTML = content;
-        },
-        error: function (err) {
-            console.log(err)
-            // lỗi
-        }
+        document.getElementById("P_id").innerHTML = content;
+    },
+    error: function (err) {
+        console.log(err)
+        // lỗi
+    }
 });
 
-function search() {
-    let number1 = document.getElementById("number1").value;
-    let number2 = document.getElementById("number2").value;
-    let category = document.getElementById("C_id").value;
-    let provider = document.getElementById("P_id").value;
-    let name = document.getElementById("name").value;
-    let search = {
-        number1: number1,
-        number2: number2,
-        category:category,
-        provider:provider,
-        name: name
+$.ajax({
+    type: "GET",
+    url: "http://localhost:8080/api/category",
+    success: function (data) {
+        console.log(data);
+        let content = "";
+        for (let i = 0; i < data.length; i++) {
+            content += `<option></option><br><option value=${data[i].id}>${data[i].name}</option>`
+        }
+        document.getElementById("C_id").innerHTML = content;
+    },
+    error: function (err) {
+        console.log(err)
+        // lỗi
     }
+});
 
-    let formData = new FormData()
-    formData.append("search", new Blob([JSON.stringify(search)], {type: "application/json"}))
-    $.ajax({
-        type: "POST",
-        data: formData,
-        processData: false,
-        contentType: false,
-        url: `http://localhost:8080/api/search`,
-        success: function (data) {
-            let content = "<table style=\"width: auto\">\n" +
-                "    <tr>\n" +
-                "      <th>Stt</th>\n" +
-                "      <th>Tên sản phẩm </th>\n" +
-                "      <th>Miêu tả </th>\n" +
-                "      <th>Số lượng </th>\n" +
-                "      <th>Giá </th>\n" +
-                "      <th>Loại sản phẩm </th>\n" +
-                "      <th>Nhà cung cấp </th>\n" +
-                "      <th>Ảnh minh họa </th>\n" +
-                "    </tr>";
-            for (let i = 0; i < data.length; i++) {
-                content += `<tr>
+function showData(data) {
+    let content = "<table style=\"width: auto\">\n" +
+        "    <tr>\n" +
+        "      <th>Stt</th>\n" +
+        "      <th>Tên sản phẩm </th>\n" +
+        "      <th>Miêu tả </th>\n" +
+        "      <th>Số lượng </th>\n" +
+        "      <th>Giá </th>\n" +
+        "      <th>Loại sản phẩm </th>\n" +
+        "      <th>Nhà cung cấp </th>\n" +
+        "      <th>Ảnh minh họa </th>\n" +
+        "    </tr>";
+    for (let i = 0; i < data.length; i++) {
+        content += `<tr>
                         <td>${i + 1}</td>
                         <td>${data[i].name}</td>
                         <td>${data[i].description}</td>
@@ -79,12 +57,496 @@ function search() {
                         <td><button onclick="updateProduct(${data[i].id})"> update</button></td>
                         <td><button onclick="deleteProduct(${data[i].id})"> delete</button></td>
                     </tr>`
-            }
-            document.getElementById("display").innerHTML = content;
-        },
-        error: function (err) {
-            console.log(err)
-            // lỗi
+    }
+    return content;
+}
+
+function search() {
+    let number1 = document.getElementById("number1").value;
+    let number2 = document.getElementById("number2").value;
+    let C_id = document.getElementById("C_id").value;
+    let P_id = document.getElementById("P_id").value;
+    let name = document.getElementById("name").value;
+
+    if (number1 !== "" && number2 !== "" && C_id !== "" &&  P_id !== "" && name !=="") {
+        let search = {
+            number1: number1,
+            number2: number2,
+            category:
+                {
+                    id: C_id
+                },
+            provider: {
+                id: P_id
+            },
+            name: name
         }
-    })
+        $.ajax({
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            type: "POST",
+            data: JSON.stringify(search),
+            url: "http://localhost:8080/api/search",
+            success: function (data) {
+                let content = showData(data);
+                document.getElementById("display").innerHTML = content;
+            },
+            error: function (err) {
+                console.log(err)
+                // lỗi
+            }
+        })
+
+        // search 1: A B C
+    } else if (number1 !== "" && number2 !== "" && C_id !== "" &&  P_id !== "" && name ==="") {
+        let search = {
+            number1: number1,
+            number2: number2,
+            category:
+                {
+                    id: C_id
+                },
+            provider: {
+                id: P_id
+            },
+            name: "default"
+        }
+        $.ajax({
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            type: "POST",
+            data: JSON.stringify(search),
+            url: "http://localhost:8080/api/search/search1",
+            success: function (data) {
+                let content = showData(data);
+                document.getElementById("display").innerHTML = content;
+            },
+            error: function (err) {
+                console.log(err)
+                // lỗi
+            }
+        })
+
+        // search 2: A C D
+    } else if (number1 !== "" && number2 !== "" && C_id === "" &&  P_id !== "" && name !=="") {
+        let search = {
+            number1: number1,
+            number2: number2,
+            category:
+                {
+                    id: 1
+                },
+            provider: {
+                id: P_id
+            },
+            name: name
+        }
+        $.ajax({
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            type: "POST",
+            data: JSON.stringify(search),
+            url: "http://localhost:8080/api/search/search2",
+            success: function (data) {
+                let content = showData(data);
+                document.getElementById("display").innerHTML = content;
+            },
+            error: function (err) {
+                console.log(err)
+                // lỗi
+            }
+        })
+
+        // search 3: A B D
+    } else if (number1 !== "" && number2 !== "" && C_id !== "" &&  P_id === "" && name !=="") {
+        let search = {
+            number1: number1,
+            number2: number2,
+            category:
+                {
+                    id: C_id
+                },
+            provider: {
+                id: 1
+            },
+            name: name
+        }
+        $.ajax({
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            type: "POST",
+            data: JSON.stringify(search),
+            url: "http://localhost:8080/api/search/search3",
+            success: function (data) {
+                let content = showData(data);
+                document.getElementById("display").innerHTML = content;
+            },
+            error: function (err) {
+                console.log(err)
+                // lỗi
+            }
+        })
+
+        // search 4: BCD
+    } else if (number1 === "" && number2 === "" && C_id !== "" &&  P_id !== "" && name !=="") {
+        let search = {
+            number1: 0,
+            number2: 0,
+            category:
+                {
+                    id: C_id
+                },
+            provider: {
+                id: P_id
+            },
+            name: name
+        }
+        $.ajax({
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            type: "POST",
+            data: JSON.stringify(search),
+            url: "http://localhost:8080/api/search/search4",
+            success: function (data) {
+                let content = showData(data);
+                document.getElementById("display").innerHTML = content;
+            },
+            error: function (err) {
+                console.log(err)
+                // lỗi
+            }
+        })
+
+        // search 5: AB
+    } else if (number1 !== "" && number2 !== "" && C_id !== "" &&  P_id === "" && name ==="") {
+        let search = {
+            number1: number1,
+            number2: number2,
+            category:
+                {
+                    id: C_id
+                },
+            provider: {
+                id: 1
+            },
+            name: "default"
+        }
+        $.ajax({
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            type: "POST",
+            data: JSON.stringify(search),
+            url: "http://localhost:8080/api/search/search5",
+            success: function (data) {
+                let content = showData(data);
+                document.getElementById("display").innerHTML = content;
+            },
+            error: function (err) {
+                console.log(err)
+                // lỗi
+            }
+        })
+
+        // search 6: A C
+    } if (number1 !== "" && number2 !== "" && C_id === "" &&  P_id !== "" && name ==="") {
+        let search = {
+            number1: number1,
+            number2: number2,
+            category:
+                {
+                    id: 1
+                },
+            provider: {
+                id: P_id
+            },
+            name: "Default"
+        }
+        $.ajax({
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            type: "POST",
+            data: JSON.stringify(search),
+            url: "http://localhost:8080/api/search/search6",
+            success: function (data) {
+                let content = showData(data);
+                document.getElementById("display").innerHTML = content;
+            },
+            error: function (err) {
+                console.log(err)
+                // lỗi
+            }
+        })
+
+        // search 7: A D
+    } else if (number1 !== "" && number2 !== "" && C_id === "" &&  P_id === "" && name !=="") {
+        let search = {
+            number1: number1,
+            number2: number2,
+            category:
+                {
+                    id: 1
+                },
+            provider: {
+                id: 1
+            },
+            name: name
+        }
+        $.ajax({
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            type: "POST",
+            data: JSON.stringify(search),
+            url: "http://localhost:8080/api/search/search7",
+            success: function (data) {
+                let content = showData(data);
+                document.getElementById("display").innerHTML = content;
+            },
+            error: function (err) {
+                console.log(err)
+                // lỗi
+            }
+        })
+
+        // search 8: BC
+    } else if (number1 === "" && number2 === "" && C_id !== "" &&  P_id !== "" && name ==="") {
+        let search = {
+            number1: 0,
+            number2: 0,
+            category:
+                {
+                    id: C_id
+                },
+            provider: {
+                id: P_id
+            },
+            name: "Default"
+        }
+        $.ajax({
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            type: "POST",
+            data: JSON.stringify(search),
+            url: "http://localhost:8080/api/search/search8",
+            success: function (data) {
+                let content = showData(data);
+                document.getElementById("display").innerHTML = content;
+            },
+            error: function (err) {
+                console.log(err)
+                // lỗi
+            }
+        })
+
+        // search 9: BD
+    } else if (number1 === "" && number2 === "" && C_id !== "" &&  P_id === "" && name !=="") {
+        let search = {
+            number1: 0,
+            number2: 0,
+            category:
+                {
+                    id: C_id
+                },
+            provider: {
+                id: 1
+            },
+            name: name
+        }
+        $.ajax({
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            type: "POST",
+            data: JSON.stringify(search),
+            url: "http://localhost:8080/api/search/search9",
+            success: function (data) {
+                let content = showData(data);
+                document.getElementById("display").innerHTML = content;
+            },
+            error: function (err) {
+                console.log(err)
+                // lỗi
+            }
+        })
+
+        // search 10: CD
+    } else if (number1 === "" && number2 === "" && C_id === "" &&  P_id !== "" && name !=="") {
+        let search = {
+            number1: 0,
+            number2: 0,
+            category:
+                {
+                    id: 1
+                },
+            provider: {
+                id: P_id
+            },
+            name: name
+        }
+        $.ajax({
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            type: "POST",
+            data: JSON.stringify(search),
+            url: "http://localhost:8080/api/search/search10",
+            success: function (data) {
+                let content = showData(data);
+                document.getElementById("display").innerHTML = content;
+            },
+            error: function (err) {
+                console.log(err)
+                // lỗi
+            }
+        })
+
+        // search 11: A
+    } else if (number1 !== "" && number2 !== "" && C_id === "" &&  P_id === "" && name ==="") {
+        let search = {
+            number1: number1,
+            number2: number2,
+            category:
+                {
+                    id: 1
+                },
+            provider: {
+                id: 1
+            },
+            name: "Default"
+        }
+        $.ajax({
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            type: "POST",
+            data: JSON.stringify(search),
+            url: "http://localhost:8080/api/search/search11",
+            success: function (data) {
+                let content = showData(data);
+                document.getElementById("display").innerHTML = content;
+            },
+            error: function (err) {
+                console.log(err)
+                // lỗi
+            }
+        })
+
+        // search 12: B
+    } else if (number1 === "" && number2 === "" && C_id !== "" &&  P_id === "" && name ==="") {
+        let search = {
+            number1: 0,
+            number2: 0,
+            category:
+                {
+                    id: C_id
+                },
+            provider: {
+                id: 1
+            },
+            name: "Default"
+        }
+        $.ajax({
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            type: "POST",
+            data: JSON.stringify(search),
+            url: "http://localhost:8080/api/search/search12",
+            success: function (data) {
+                let content = showData(data);
+                document.getElementById("display").innerHTML = content;
+            },
+            error: function (err) {
+                console.log(err)
+                // lỗi
+            }
+        })
+
+        // search 13: C
+    } else if (number1 === "" && number2 === "" && C_id === "" &&  P_id !== "" && name ==="") {
+        let search = {
+            number1: 0,
+            number2: 0,
+            category:
+                {
+                    id: 0
+                },
+            provider: {
+                id: P_id
+            },
+            name: "Default"
+        }
+        $.ajax({
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            type: "POST",
+            data: JSON.stringify(search),
+            url: "http://localhost:8080/api/search/search13",
+            success: function (data) {
+                let content = showData(data);
+                document.getElementById("display").innerHTML = content;
+            },
+            error: function (err) {
+                console.log(err)
+                // lỗi
+            }
+        })
+
+        // search 14: D
+    } else if (number1 === "" && number2 === "" && C_id === "" &&  P_id === "" && name !=="") {
+        let search = {
+            number1: 0,
+            number2: 0,
+            category:
+                {
+                    id: 1
+                },
+            provider: {
+                id: 1
+            },
+            name: name
+        }
+        $.ajax({
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            type: "POST",
+            data: JSON.stringify(search),
+            url: "http://localhost:8080/api/search/search14",
+            success: function (data) {
+                let content = showData(data);
+                document.getElementById("display").innerHTML = content;
+            },
+            error: function (err) {
+                console.log(err)
+                // lỗi
+            }
+        })
+    } else {
+        alert("Đéo tìm thấy nhé!")
+    }
 }
