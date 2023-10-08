cea037f4c7cb1793a68456a8ea0049ceccfcef08
nguyendong <nguyendong220496@gmail.com>
2023-10-08 20:26:56 +0700

    cart thanh toan

--- src/main/java/com/example/modul4_tmdt_group4/service/IBillService.java
+++ src/main/java/com/example/modul4_tmdt_group4/service/IBillService.java
@@ -3,4 +3,5 @@ package com.example.modul4_tmdt_group4.service;
 import com.example.modul4_tmdt_group4.model.Bill;
 
 public interface IBillService extends IService<Bill> {
+    Long createBill(Bill bill);
 }
